9edfd361b83de241d02622fdb43970bafd7bb305
De La Bastida Rodríguez Johan Mateo <jodelabastidaro@uide.edu.ec>
2026-05-22 01:48:57 +0000

    wip: s3

---
+++ s3_copia.sh
@@ -0,0 +1,48 @@
+#print working directory
+#/workspaces/intro-unix-may-2025-online
+pwd
+#list
+ls
+#make directory
+mkdir
+#change directory 
+mkdir padre
+# change directory padre 
+cd padre
+#mostrar todo el contenido y el oculto opcion extendida y abreviada
+ls --all
+ls -a
+ls --help
+#me muevo al directorio que contiene al directorio actual
+cd ..
+#mi directorio actual es .
+cd .
+#
+man ls
+#nuevo archivo
+touch nuevo_archivo
+#ruta absoluta
+cd /workspaces/intro-unix-may-2025-online/padre 
+#raiz de todo los directorios
+cd /
+#
+cd workspaces/
+cd intro-unix-may-2025-online/
+cd padre
+# nuevo archivo
+touch hijo
+touch HIJO
+mkdir PADRE
+HOLA="chao"
+#variable normal
+echo $HOLA
+#variable de entorno
+echo #HOME
+printf "mensaje\n"
+echo "mensaje"
+#desde cualquier ubicacion me lleva al directorio home
+cd ~
+#remove directory elimina el directorio PADRE
+mrdir PADRE/
+#remove elimina archivos
+rm nuevo archivo
